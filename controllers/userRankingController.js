237c31a6cacbbf9bdb62f550855353998933a28b
wwha2121 <wwha2121@gmail.com>
2025-05-28 15:42:11 +0900

    return res.status

--- controllers/userRankingController.js
+++ controllers/userRankingController.js
@@ -3,8 +3,9 @@ const userRankingService = require('../services/userRankingService');
 exports.getUserRanking = async (res) => {
     try {
         const userRanking = userRankingService.userRanking;
-        return res.status(200).json(userRanking);
+
+        res.status(200).json(userRanking);
     } catch (error) {
-        return res.status(500).json({ error: '서버 오류', details: error.message });
+        res.status(500).json({ error: '서버 오류', details: error.message });
     }
 };
